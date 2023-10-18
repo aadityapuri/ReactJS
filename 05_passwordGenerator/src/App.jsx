56b25500e966c3e361de81230db9361a957a2a03
Aaditya <aadityapuriaaditya@gmail.com>
2023-10-18 23:14:54 +0530

    Password Generator Project

---
+++ 05_passwordGenerator/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState, useCallback } from 'react'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [num, setNum] = useState(false)
+  const [char, setChar] = useState(false)
+  const [pass, setPass] = useState("")
+
+  const passGen= useCallback(()=>{
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (num) str += "0123456789"
+    if (char) str += "`~!@#$%^&*()_-+={}[]|\;'\"<>?/";
+
+    for(let i=0; i < length; i++){
+      let ran = Math.floor(Math.random()*str.length + 1);
+      pass += str.charAt(ran);
+    }
+    setPass(pass);
+  }, [length, num, char, setPass]);
+
+  return (
+    <>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700'>
+    <h1 className='text-white text-center my-3'>Password generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input type='text' value={pass} className='outline-none w-full py-1 px-3' placeholder='password' readOnly />
+        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-canter gap-x-1'>
+          <input type='range' min={6} max={100} value={length} className='cursor-pointer' onChange={(e)=>{setLength(e.target.value)}} />
+          <label>Length: {length}</label>
+        </div>
+        
+        <div className='flex items-canter gap-x-1'>
+        <input type='checkbox' defaultChecked={num} onChange={()=>{setNum((prev)=>!prev)}} />
+          <label>Numbers</label>
+        </div>
+        
+        <div className='flex items-canter gap-x-1'>
+        <input type='checkbox' defaultChecked={char} onChange={()=>{setChar((prev)=>!prev)}} />
+          <label>Characters</label>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
