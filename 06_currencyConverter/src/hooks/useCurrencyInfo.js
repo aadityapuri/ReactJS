53aedda1659893008c74a640ce8f9c9fad758d7d
Aaditya <aadityapuriaaditya@gmail.com>
2023-10-21 09:49:13 +0530

    Made Changes and fixed Currency Converter

--- 06_currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06_currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 
+
 function useCurrencyInfo(currency){
   const [data, setData] = useState({})
   let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-  
   useEffect(()=>{
     fetch(url)
     .then((response)=>response.json())
     .then((response)=>setData(response[currency]))
   }, [currency]);
+  console.log(data);
   return data;
 }
 
