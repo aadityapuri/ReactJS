73a3b7e58e3f07d3a0bef87c57d6f92c9f68dc17
Aaditya <aadityapuriaaditya@gmail.com>
2024-02-03 15:42:28 +0530

    Made basic changes and refined the code

--- 03_bg-changer/src/App.jsx
+++ 03_bg-changer/src/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react"
 
 function App() {
+  function CustomButton({color}){
+    let colorName = color.toString().charAt(0).toUpperCase() + color.slice(1);
+    return(
+      <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: color}} onClick={()=>setColor(color)}>{colorName}</button>
+    )
+  }
   const [color, setColor] = useState("olive")
 
   return (
     <div className="w-full h-screen" style = {{backgroundColor: color}}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shado-lg bg-white px-3 py-2 rounded-3xl">
-          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "red"}} onClick={()=>setColor("red")}>Red</button>
-          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "green"}} onClick={()=>setColor("green")}>Green</button>
-          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "blue"}} onClick={()=>setColor("blue")}>Blue</button>
-          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "black"}} onClick={()=>setColor("black")}>Black</button>
-          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "purple"}} onClick={()=>setColor("purple")}>Purple</button>
+          <CustomButton color={"red"} />
+          <CustomButton color={"green"} />
+          <CustomButton color={"blue"} />
+          <CustomButton color={"black"} />
+          <CustomButton color={"purple"} />
         </div>
       </div>
     </div>
